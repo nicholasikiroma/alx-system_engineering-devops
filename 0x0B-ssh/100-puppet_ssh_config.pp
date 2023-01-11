88f6c53ecfbe5d6eb28a923eeb769fd022f46196
Nicholas <nicholasikiroma@gmail.com>
2023-01-11 12:39:24 -0800

    puppet config fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,13 +5,13 @@ include stdlib
 file_line { 'Turn off passwd auth':
   ensure  =>  present,
   path    =>  '/etc/ssh/ssh_config',
-  line    =>  '  PasswordAuthentication no',
-  replace => true
+  line    =>  '    PasswordAuthentication no',
+  replace => true,
 }
 
 file_line { 'Declare identity file':
   ensure  =>  present,
   path    =>  '/etc/ssh/ssh_config',
-  line    =>  '	 IdentifyFile ~/.ssh/school',
-  replace =>  true
+  line    =>  '    IdentifyFile ~/.ssh/school',
+  replace =>  true,
 }
